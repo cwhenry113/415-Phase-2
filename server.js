8e928ea2053e79f3b4822e5d45bd23e69674971b
cwhenry113 <carter.henry@selu.edu>
2023-04-29 00:16:50 -0500

    phase 3

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const router = express.Router();
 const { Client } = require('pg');
+const convert = require('xml-js');
+require('body-parser-xml')(bodyParser);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
 app.listen(3000);
@@ -46,6 +49,13 @@ router.get('/rest/ticket/id', async function (req, res) {
     res.send(await clientGetOne())
 });
 
+//Adapts JSON output to XML for the GET rest/ticket/id route
+router.get('/rest/xml/ticket/id', async function (req, res) {
+    text = 'SELECT * FROM tickets WHERE id=$1'
+    values = [req.body.id]
+    const toAdapt = await clientGetOne()
+    res.send(convert.json2xml(toAdapt, {compact: true, spaces: 4}))
+});
 
 //Create a new ticket
 router.post('/rest/ticket', async function (req, res) {
@@ -65,5 +75,12 @@ router.delete('/rest/ticket/id', async function (req, res) {
 router.put('/rest/ticket/id', async function (req, res) {
     text = 'UPDATE tickets SET type=$1, subject=$2, description=$3, status=$4 WHERE id=$5'
     values = [req.body.type, req.body.subject, req.body.description, req.body.status, req.body.id]
+    res.send(await clientGetOne())
+});
+
+//Adapts XML input to JSON for the PUT rest/ticket/id route
+router.put('/rest/xml/ticket/id', bodyParser.xml(), async function (req, res) {
+    text = 'UPDATE tickets SET type=$1, subject=$2, description=$3, status=$4 WHERE id=$5'
+    values = [req.body.root.type[0], req.body.root.subject[0], req.body.root.description[0], req.body.root.status[0], req.body.root.id[0]]
     res.send(await clientGetOne())
 });
